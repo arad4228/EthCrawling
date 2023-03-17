93d11678105519c84d6bc0b5bd1afbe631684cf7
arad4228 <arad4228@gmail.com>
2023-03-17 22:28:24 +0900

    [KyoungJae]Add get TransactionField

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import requests
 import re
 import time
 
+
+TX_List = []
+TX_Status_List = []
 From_List = []
 From_Tag_List = []
 To_List = []
@@ -16,20 +19,52 @@ To_Tag_List = []
 
 base_Url = "https://etherscan.io/txs"
 index = 1
-max_Index = 10000
+max_Index = 5000
 
 # 50 to 100 Crawling
 response = requests.get(base_Url, params=params, cookies=cookies, headers=headers, data=data)
 
-for page in range(1):
+for page in range(5):
     # html 파일 받기
     response = requests.get(base_Url, params=params, cookies=cookies, headers=headers)
-    text = response.text
+    text = response.text.replace('​','')
 
     # 파일 단위 저장기능(for Debug)
     # test_html = open("index.html","w", encoding='utf8')
     # test_html.write(text)
     # test_html.close()
+
+    element = 0
+
+    # 정규식
+    # 최초: (<span class="text-danger"[\w\s!@#$%%^&*()_,.<>/\"\'+:;=-]*<\/span>\s)?<span class="hash-tag text-truncate">\s<a href="\/tx[\w/]*"[\w\s!@#$%%^&*()_,.>/\"\'+:;=-]*<\/a>
+    # 수정: (<span class="text-danger"[\w\s!@#$%%^&*()_,.<>/\"\'+:;=-]*<\/span>\s)?<span class="hash-tag text-truncate">\s+<a href="\/tx[\w/]*"[\w\s!@#$%%^&*()_,.>/\"\'+:;=-]*<\/a>
+    tx_List = re.finditer(
+        r'(<span class="text-danger"[\w\s!@#$%%^&*()_,.<>/\"\'+:;=-]*<\/span>\s)?<span class="hash-tag text-truncate">\s+<a href="\/tx[\w/]*"[\w\s!@#$%%^&*()_,.>/\"\'+:;=-]*<\/a>',
+    text)
+
+    # TX
+    for tx in tx_List:
+        # group를 통해서 찾은 결과 접근
+        matched_Str = tx.group()
+        #  + " find: " + matched_Str
+        print("num: " + str(element))
+
+        find_TxAddress = re.search(r'tx[\w/]*', matched_Str).group().replace('tx/','')
+        check_Status = re.search(r'<span class="text-danger"[\w\s!@#$%%^&*()_,.<>/\"\'+:;=-]*<\/span>\s', matched_Str)
+
+        TX_List.append(find_TxAddress)
+
+        if check_Status is None:
+            TX_Status_List.append("None")
+        # 정규식
+        # data-bs-title="[\w\s!@#$%%^&*()_,.<>/\'+:;=-]*" : 에러 문구 추출
+        else:
+            refine_Str = re.search(r'data-bs-title="[\w\s!@#$%%^&*()_,.<>/\'+:;=-]*"', check_Status.group()).group().replace('data-bs-title="','')
+            refine_Str = refine_Str.replace('"','')
+            TX_Status_List.append(refine_Str)
+        element = element + 1
+
 
     # 정규식
     # <a href="\/address\/[\w]*\" class=\"[\w\s-]*" [\w\s-=":]*(<br\/>)?(\([\w]*\))?">[\s\w:.?!@#$%^&*()_+=+/*-]*
@@ -38,45 +73,51 @@ for page in range(1):
     # 최종: <a href="\/address[\w/]*" class="(hash-tag text-truncate)?"[\s\w=:"!@#$%^&*_=+~`.()-]*(<br\/>\(\w*\)")?
     # 위 정규식의 문제점은 내부의 하나만 문제가 발생해도 안긁힘.(수정이 필요하다.)
     # 현재 적용: <a href="\/address[\w/]*"[\w\s!@#$%%^&*()_,.<>/"'+:;=-]*<\/a>
+    # From, To Address 추출
     address_List = re.finditer(
         r'<a href="\/address[\w/]*"[\w\s!@#$%%^&*()_,.<>/\"\'+:;=-]*<\/a>',
         text)
 
     element = 0
 
+    # From, To Address
     for address in address_List:
-        # group을 통해서 찾은 결과 접근
-        matched_str = address.group()
-        print("num: " + str(element) + " find: " + matched_str)
+        matched_Str = address.group()
+        #  + " find: " + matched_Str
+        print("num: " + str(element))
 
-        find_address = re.search(r'address[\w/]*', matched_str).group().replace('address/', '')
+        find_Address = re.search(r'address[\w/]*', matched_Str).group().replace('address/', '')
         # 최종: data-bs-title="(Public Tag: )?[\w\s.:!@#$%^&_=+,/?-]*
-        check_tag = re.search(r'data-bs-title="(Public Tag: )?[\w\s.:!@#$%^&_=+,/?-]*', matched_str).group().replace('data-bs-title="', '')
+        check_tag = re.search(r'data-bs-title="(Public Tag: )?[\w\s.:!@#$%^&_=+,/?-]*', matched_Str).group().replace('data-bs-title="', '')
 
         # From
         if element % 2 == 0:
-            From_List.append(find_address)
+            From_List.append(find_Address)
 
-            if check_tag != find_address:
+            if check_tag != find_Address:
                 From_Tag_List.append(check_tag)
             else:
                 From_Tag_List.append('None')
         # To
         elif element % 2 == 1:
-            To_List.append(find_address)
+            To_List.append(find_Address)
 
-            if check_tag != find_address:
+            if check_tag != find_Address:
                 To_Tag_List.append(check_tag)
             else:
                 To_Tag_List.append('None')
         element = element + 1
 
-    # 10초간 휴식
-    time.sleep(10)
-
+    # 3페이지 이후 5초 휴식
+    if page % 3 == 0:
+        time.sleep(5)
+        print("Sleep 5 sec")
+    print("Current Page is " + str(page) + " Done")
 
 Ethereum_CSV = pd.DataFrame (
     {
+        'TXN': TX_List,
+        'TX_STATUS': TX_Status_List,
         'From': From_List,
         'From_TAG': From_Tag_List,
         'To': To_List,
